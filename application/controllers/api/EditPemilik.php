050e1be4729dd062342802b2f904b6bd12c67d28
Pradhipta Bagaskara <pradhiptabagaskara@gmail.com>
2020-03-06 18:26:37 +0700

    fix typo

--- application/controllers/api/EditPemilik.php
+++ application/controllers/api/EditPemilik.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
 use Restserver\Libraries\REST_Controller;
 
-class EditPemilik extends REST_Controller {
+class Editpemilik extends REST_Controller {
 	public function __construct()
 	{
 		parent::__construct();
